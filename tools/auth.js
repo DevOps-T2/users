268bbd9550ccaaa8ef2d6c68452e766aa91cf611
LukaZeleznik <Luka.zeleznik10@gmail.com>
2021-12-20 21:31:14 +0100

    am dum

--- tools/auth.js
+++ tools/auth.js
@@ -104,7 +104,7 @@ async function createNewUser(email, password, displayName, userRole) {
 
     user.save(function (err) {
         if (err){
-            return({errors: err);
+            return({errors: err});
         }
         return user;
     });  
